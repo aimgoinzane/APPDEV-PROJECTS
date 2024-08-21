1ad91159b5f021b3cd8ea185a90f433fdba0dc23
lance pablo <pablo_l@f112.lan>
2024-08-21 16:18:45 +0800

    added counter and navbar woooo

---
+++ JSProjects/Counter/app.js
@@ -0,0 +1,40 @@
+//set initial value for our count variable 
+let count = 0;
+//select the value buttons
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll('.btn')
+btns.forEach((btn) => {
+    //conesole.log(btn) 
+    btn.addEventListener('click', (e) => {
+        const styles = e.currentTarget.classList
+        if(styles.contains('decrease')){
+            count--
+        }
+        else if(styles.contains('increase')){
+            count++
+        }
+        else if(styles.contains('plus5')){
+            count += 5
+        }
+        else if(styles.contains('minus5')){
+            count -= 5
+        }
+        else if(styles.contains('randomize')){
+            count = Math.floor((Math.random() * 2 - 1) * 100)
+        }
+        else {
+            count = 0
+        }
+        if(count > 0) {
+            value.style.color = "green"
+        }
+        if(count < 0) {
+            value.style.color = "red"
+        }
+        if(count == 0) {
+            value.style.color = "black"
+        }
+
+        value.textContent = count
+    })  
+})
